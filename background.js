3b9e11561332beea29b983363d97a97ddbe87eb0
D <dsheshank06@gmail.com>
2026-06-29 22:48:56 +0530

    Store last visited URL

--- background.js
+++ background.js
@@ -2,6 +2,14 @@ console.log("ResumeMark Loaded");
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url) {
-        console.log("Visited:", tab.url);
+
+        chrome.storage.local.set({
+            lastVisitedUrl: tab.url
+        });
+
+        console.log("Saved:", tab.url);
     }
+});
+chrome.storage.local.get("lastVisitedUrl", (result) => {
+    console.log("Stored URL:", result.lastVisitedUrl);
 });
